01a3094c8c98a26170294829434a6b976fcdc6da
Xiaomi Team <112295130+XiaomiBOT@users.noreply.github.com>
2022-09-20 11:01:10 +0700

    Update xiaomihi.js

--- plugins/xiaomihi.js
+++ plugins/xiaomihi.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let hallo = fs.readFileSync('./mp3/DJ GANI GANI X HUJAN BADAI ANGIN RIBUT | BY: @gobelll.mp3') 
+let hallo = fs.readFileSync('./mp3/PTT-20220920-WA0026.opus') 
 conn.sendFile(m.chat, hallo, '', '', m, true)
 //conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
